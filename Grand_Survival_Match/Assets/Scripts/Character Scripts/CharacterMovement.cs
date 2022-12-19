16f65964c774ff1492838b1438aa716fdb29063a
LeeKiHwan <s22049@gsm.hs.kr>
2022-12-19 20:53:59 +0900

    캐릭터 이동 기능 추가 및 무기 에셋 추가
    - 마우스 좌클릭 시 마우스 방향으로 이동 및 애니메이션 추가
    - LowPoly Sword 에셋 추가

---
+++ Grand_Survival_Match/Assets/Scripts/Character Scripts/CharacterMovement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Photon.Pun;
+
+public class CharacterMovement : MonoBehaviour
+{
+    NavMeshAgent agent;
+
+    public float rotateSpeedMovement = 0.1f;
+    float rotateVelocity;
+
+    public PhotonView PV;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PV.IsMine)
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                RaycastHit hit;
+
+                if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
+                {
+                    agent.SetDestination(hit.point);
+
+                    Quaternion rotationToLookAt = Quaternion.LookRotation(hit.point - transform.position);
+                    float rotationY = Mathf.SmoothDampAngle(transform.eulerAngles.y, rotationToLookAt.eulerAngles.y, ref rotateVelocity, rotateSpeedMovement * (Time.deltaTime * 5));
+
+                    transform.eulerAngles = new Vector3(0, rotationY, 0);
+                }
+            }
+        }
+    }
+}
